b3c5a37bd23b7cd3d75c8e995b502cccee3d6cbf
bhickta <61617746+bhickta@users.noreply.github.com>
2022-08-31 22:49:44 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 
 for x in $(cat urls.txt)
 do
-    python /home/runner/work/YouTube-video-to-PDF-PPT-Images/YouTube-video-to-PDF-PPT-Images/YouTube-video-to-PDF-PPT-Images/main.py --skip 10 --file images --url $x
+    python /home/runner/work/YouTube-video-to-PDF-PPT-Images/YouTube-video-to-PDF-PPT-Images/YouTube-video-to-PDF-PPT-Images/main.py --skip 10 --silent_del true --file images --url $x
 done
